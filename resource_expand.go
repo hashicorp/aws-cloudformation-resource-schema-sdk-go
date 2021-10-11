622c2e21c08706a10d648476ed8fb5b3172819d6
Kit Ewbank <Kit_Ewbank@hotmail.com>
2021-10-11 17:28:35 -0400

    Allow arbitrary levels of nesting when expanding Definition and Property JSON Pointer references.

--- resource_expand.go
+++ resource_expand.go
@@ -29,23 +29,10 @@ func (r *Resource) Expand() error {
 }
 
 // ResolveProperties resolves all References in a top-level name-to-property map.
-// In unresolved form nested properties are not allowed so we don't need to recurse.
+// In theory unresolved form nested properties are not allowed but in practice they do occur,
+// so support arbitrarily deeply nested references.
 func (r *Resource) ResolveProperties(properties map[string]*Property) error {
 	for propertyName, property := range properties {
-		resolved, err := r.ResolveProperty(property)
-
-		if err != nil {
-			return fmt.Errorf("error resolving %s: %w", propertyName, err)
-		}
-
-		if resolved {
-			// For example:
-			// "Configuration": {
-			//   "$ref": "#/definitions/ClusterConfiguration"
-			// },
-			continue
-		}
-
 		switch property.Type.String() {
 		case PropertyTypeArray:
 			// For example:
@@ -55,125 +42,57 @@ func (r *Resource) ResolveProperties(properties map[string]*Property) error {
 			//     "$ref": "#/definitions/CapacityProviderStrategyItem"
 			//   }
 			// },
-			_, err = r.ResolveProperty(property.Items)
+			_, err := r.ResolveProperty(property.Items)
 
 			if err != nil {
 				return fmt.Errorf("error resolving %s Items: %w", propertyName, err)
 			}
+
 		case PropertyTypeObject:
-			for objPropertyName, objProperty := range property.Properties {
-				resolved, err := r.ResolveProperty(objProperty)
+			// For example:
+			// "ClusterConfiguration": {
+			//   "type": "object",
+			//   "properties": {
+			//     "ExecuteCommandConfiguration": {
+			//       "$ref": "#/definitions/ExecuteCommandConfiguration"
+			//     }
+			//   }
+			// },
+			err := r.ResolveProperties(property.Properties)
 
-				if err != nil {
-					return fmt.Errorf("error resolving %s Property (%s): %w", propertyName, objPropertyName, err)
-				}
-
-				if resolved {
-					// For example:
-					// "ClusterConfiguration": {
-					//   "type": "object",
-					//   "properties": {
-					//     "ExecuteCommandConfiguration": {
-					//       "$ref": "#/definitions/ExecuteCommandConfiguration"
-					//     }
-					//   }
-					// },
-					continue
-				}
-
-				switch objProperty.Type.String() {
-				case PropertyTypeArray:
-					// For example:
-					// "LambdaContainerParams": {
-					//   "type": "object",
-					//   "properties": {
-					//     "Volumes": {
-					//       "type": "array",
-					//       "items": {
-					//         "$ref": "#/definitions/LambdaVolumeMount"
-					//       }
-					//     }
-					//   }
-					// },
-					_, err = r.ResolveProperty(objProperty.Items)
-
-					if err != nil {
-						return fmt.Errorf("error resolving %s Property (%s) Items: %w", propertyName, objPropertyName, err)
-					}
-				case PropertyTypeObject:
-					// Pragmatically resolve any References at this level even though they are not allowed.
-					for objPropertyName2, objProperty2 := range objProperty.Properties {
-						_, err := r.ResolveProperty(objProperty2)
-
-						if err != nil {
-							return fmt.Errorf("error resolving %s Property (%s) Property (%s): %w", propertyName, objPropertyName, objPropertyName2, err)
-						}
-					}
-
-					for pattern, patternProperty := range objProperty.PatternProperties {
-						// For example:
-						// "LambdaFunctionRecipeSource": {
-						//   "type": "object",
-						//   "properties": {
-						//     "ComponentDependencies": {
-						//       "type": "object",
-						//       "patternProperties": {
-						//         "": {
-						//           "$ref": "#/definitions/ComponentDependencyRequirement"
-						//         }
-						//       }
-						//     }
-						//   }
-						// },
-						_, err = r.ResolveProperty(patternProperty)
-
-						if err != nil {
-							return fmt.Errorf("error resolving %s Property (%s) Pattern(%s): %w", propertyName, objPropertyName, pattern, err)
-						}
-					}
-				}
+			if err != nil {
+				return fmt.Errorf("error resolving %s Properties: %w", propertyName, err)
 			}
 
-			for patternName, objProperty := range property.PatternProperties {
-				resolved, err := r.ResolveProperty(objProperty)
+			// For example:
+			// "LambdaFunctionRecipeSource": {
+			//   "type": "object",
+			//   "properties": {
+			//     "ComponentDependencies": {
+			//       "type": "object",
+			//       "patternProperties": {
+			//         "": {
+			//           "$ref": "#/definitions/ComponentDependencyRequirement"
+			//         }
+			//       }
+			//     }
+			//   }
+			// },
+			err = r.ResolveProperties(property.PatternProperties)
 
-				if err != nil {
-					return fmt.Errorf("error resolving %s pattern Property (%s): %w", propertyName, patternName, err)
-				}
+			if err != nil {
+				return fmt.Errorf("error resolving %s PatternProperties: %w", propertyName, err)
+			}
 
-				if resolved {
-					// For example:
-					// "Tags": {
-					//   "type": "object",
-					//   "patternProperties": {
-					//     "": {
-					//       "$ref": "#/definitions/TagValue"
-					//     }
-					//   }
-					// },
-					continue
-				}
+		default:
+			// For example:
+			// "Configuration": {
+			//   "$ref": "#/definitions/ClusterConfiguration"
+			// },
+			_, err := r.ResolveProperty(property)
 
-				switch objProperty.Type.String() {
-				case PropertyTypeArray:
-					// For example:
-					// "Tags": {
-					//   "type": "object",
-					//   "patternProperties": {
-					//     "": {
-					//       "type": "array",
-					//       "items": {
-					//         "$ref": "#/definitions/TagValue"
-					//       }
-					//     }
-					//   }
-					// },
-					_, err = r.ResolveProperty(objProperty.Items)
-
-					if err != nil {
-						return fmt.Errorf("error resolving %s Property (%s) Items: %w", propertyName, patternName, err)
-					}
-				}
+			if err != nil {
+				return fmt.Errorf("error resolving %s: %w", propertyName, err)
 			}
 		}
 	}
